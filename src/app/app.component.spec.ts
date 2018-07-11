79c6b338c6c97a27a663dde35c059407dc758fe9
Mark DeArmond <bobb.dearmond@gmail.com>
2018-07-10 22:55:40 -0500

    Adjusted test files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,16 +17,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as 5 ships`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.ships).toEqual(5);
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render battleship in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Battleship');
   }));
 });
