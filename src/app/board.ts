b0b77d1f45a20ee8e0ad6c405e49cb18a14b9a9d
Mark DeArmond <bobb.dearmond@gmail.com>
2018-07-10 21:20:32 -0500

    Added player and id

--- src/app/board.ts
+++ src/app/board.ts
@@ -1,5 +1,8 @@
 import { Location } from './location';
+import { Player } from './player';
 export class Board {
+	id: number;
+	player: Player;
   tiles: Object[];
 
   constructor(values: Object = {}) {
